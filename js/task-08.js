ff8247a2417bd207c4b2a1fdbea8bce84f2405b3
Ibragimov-96 <dima.ibragimov.1996@ukr.net>
2022-10-19 19:58:51 +0300

    08

--- js/task-08.js
+++ js/task-08.js
@@ -1,18 +1,36 @@
+// const form = document.querySelector(".login-form");
+// form.addEventListener("submit", onFormSubmit);
+// function onFormSubmit(event) {
+//   event.preventDefault();
+//   const obj = {};
+//   const formData = new FormData(event.currentTarget);
+//   const email = event.currentTarget.elements.email.value;
+//   const password = event.currentTarget.elements.password.value;
+//   if (email&& password) {
+//     formData.forEach((value, name) => {
+//       console.log(name ,value);
+//     });
+//     form.reset();
+//   } else {
+//     alert("Заповніть, будь ласка, всі поля форми!");
+//   }
+// }
+
+
 
 const form = document.querySelector(".login-form");
 form.addEventListener("submit", onFormSubmit);
 function onFormSubmit(event) {
   event.preventDefault();
-  const obj = [];
+  const obj = {};
   const formData = new FormData(event.currentTarget);
   const email = event.currentTarget.elements.email.value;
   const password = event.currentTarget.elements.password.value;
   if (email && password) {
-    formData.forEach((email, password) => {
-      obj.push(email,password)
-  
-      console.log(obj);
+    formData.forEach((name, value) => {
+    obj[value]=name
   });
+  console.log(obj);
     form.reset();
   } else {
     alert("Заповніть, будь ласка, всі поля форми!");
